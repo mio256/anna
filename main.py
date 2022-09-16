614860f5bb0ebfd5a096e2c00189e3663b333f7e
mio256 <reiya@woorth.io>
2022-09-16 18:11:42 +0900

    :seedling: days

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('hello')
+import datetime
+from dateutil.relativedelta import relativedelta
+
+start = datetime.datetime(year=2022, month=6, day=15)
+
+now = datetime.datetime.now()
+
+month=0
+
+while now > start + relativedelta(months=1):
+    start = start + relativedelta(months=1)
+    month += 1
+
+dt = now - start
+
+print("{} month {} day".format(month, str(dt.days)))
